a93a2be0f5d592982351d1c6c5a464a16f33d0ec
Victor Kevz <58770966+VictorKuwandira@users.noreply.github.com>
2024-08-02 04:28:58 +0300

    Modified Animation

--- src/Varinats.js
+++ src/Varinats.js
@@ -2,8 +2,8 @@ export const fadeIn = (direction, delay) => {
     return {
       hidden: {
         opacity: 0,
-        y: direction === "up" ? "100%" : direction === "down" ? "-100%" : 0,
-        x: direction === "left" ? "-100%" : direction === "right" ? "100%" : 0,
+        y: direction === "up" ? 100 : direction === "down" ? 100 : 0,
+        x: direction === "left" ? -20 : direction === "right" ? 20 : 0,
       },
       visible: {
         y: 0,
